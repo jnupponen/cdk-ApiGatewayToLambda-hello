045ea6f1d2b7d0e8a15c9f51f8f97aaf1307dc9c
Jussi Nupponen <jussi.nupponen@gofore.com>
2021-12-11 13:27:21 +0200

    Initial commit

---
+++ test/cdk-api_gateway_to_lambda-hello.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkApiGatewayToLambdaHello from '../lib/cdk-api_gateway_to_lambda-hello-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-api_gateway_to_lambda-hello-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkApiGatewayToLambdaHello.CdkApiGatewayToLambdaHelloStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
